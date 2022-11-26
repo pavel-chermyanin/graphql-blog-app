c693084206327653c4896becb0d87d011f084144
Pavel <pavel_chermyanin@mail.ru>
2022-11-26 22:47:19 +0500

    laid out the next components: PostDetail, CommentForm, Author, implemented mutation - CreatedComment

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,26 +1,51 @@
-import React from 'react'
-import { Categories, PostWidget, PostDetail, Author, Comments, CommentsForm } from "../../components";
+import React from "react";
+import {
+  Categories,
+  PostWidget,
+  PostDetail,
+  Author,
+  Comments,
+  CommentsForm,
+} from "../../components";
 import { getPosts, getPostDetails } from "../../services";
 
-const PostDetails = () => {
+const PostDetails = ({ post }) => {
   return (
-    <div className='container mx-auto px-10 mb-8'>
-        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-            <div className='col-span-1 lg:col-span-8'>
-                <PostDetail/>
-                <Author/>
-                <CommentsForm/>
-                <Comments/>
-            </div>
-            <div className='col-span-1 lg:col-span-4'>
-                <div className='relative lg:sticky top-8'>
-                    <PostWidget/>
-                    <Categories/>
-                </div>
-            </div>
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
+          <PostDetail post={post} />
+          <Author author={post.author} />
+          <CommentsForm slug={post.slug} />
+          <Comments slug={post.slug} />
         </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <PostWidget
+              slug={post.slug}
+              categories={post.categories.map((category) => category.slug)}
+            />
+            <Categories />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostDetails
+export const getStaticProps = async ({ params }) => {
+  const data = await getPostDetails(params.slug);
+  return {
+    props: { post: data },
+  };
+};
+export const getStaticPaths = async () => {
+  const posts = await getPosts()
+
+  return {
+    paths: posts.map(({node: {slug}}) => ({params: {slug}})),
+    fallback: false
+  }
+};
+
+export default PostDetails;
--- services/index.js
+++ services/index.js
@@ -37,6 +37,40 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        createdAt
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+        }
+        excerpt
+        slug
+        title
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+  return result.post;
+};
+
 export const getRecentPosts = async () => {
   const query = gql`
     query getPostDetails() {
@@ -57,7 +91,7 @@ export const getRecentPosts = async () => {
   const result = await request(graphqlAPI, query);
   return result.posts;
 };
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query getPostDetails($slug: String!, $categories: [String!]) {
       posts(
@@ -76,7 +110,7 @@ export const getSimilarPosts = async () => {
       }
     }
   `;
-  const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query, { categories, slug });
   return result.posts;
 };
 
@@ -88,7 +122,19 @@ export const getCategories = async () => {
         slug
       }
     }
-  `
+  `;
   const result = await request(graphqlAPI, query);
   return result.categories;
+};
+
+export const submitComment = async(obj) => {
+  const result = await fetch('/api/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(obj)
+  })
+
+  return result.json()
 }
